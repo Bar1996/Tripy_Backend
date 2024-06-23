8f3b95ce42a332aaa266151b8de538c97567886b
Bar1996 <bar3668@gmail.com>
2024-06-23 16:44:18 +0300

    Change to post

--- routes/plans_route.js
+++ routes/plans_route.js
@@ -6,7 +6,7 @@ const auth = require('../common/auth_middleware');
 router.post("/addPlan",auth, plans_controller.addPlan);
 router.get("/getUserPlanIds",auth, plans_controller.getUserPlanIds);
 router.post("/getPlanById", plans_controller.getPlanById);
-router.delete("/deletePlan",auth, plans_controller.deletePlan);
+router.post("/deletePlan", plans_controller.deletePlan);
 
 
 module.exports = router;
